c122b377e49fac8e402aad0d36b7be96ec5ec50d
Sebastian Castaneda <juancastag1234@gmail.com>
2023-12-20 11:30:35 -0500

    agrego el frontend, carpeta dist

--- part3/phonebookbackend/index.mjs
+++ part3/phonebookbackend/index.mjs
@@ -9,6 +9,8 @@ app.use(express.json())
 morgan.token('body', (req) => { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 app.use(cors())
+app.use(express.static('dist'))
+
 
 let persons = [
   { 
