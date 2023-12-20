28d0c2358ccd1147586c124b3e4c561ab6fcffbe
sebascasta14 <148920067+sebascasta14@users.noreply.github.com>
2023-12-20 10:41:33 -0500

    Update index.mjs

--- part3/phonebookbackend/index.mjs
+++ part3/phonebookbackend/index.mjs
@@ -33,6 +33,10 @@ let persons = [
   }
 ]
 
+app.get('/', (request, response) => {
+  response.send('hello world')
+})
+
 
 app.get('/api/persons', (request, response) => {
   response.json(persons)
