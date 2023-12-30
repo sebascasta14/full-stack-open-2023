2c736451ab90d45f6fcc59b93319d967cc6dfa87
Sebastian Castaneda <juancastag1234@gmail.com>
2023-12-30 13:29:07 -0500

    subir parte 6

---
+++ part6/query-anecdotes-main/src/NotificationContext.jsx
@@ -0,0 +1,44 @@
+import { createContext, useReducer, useContext } from 'react'
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "SET":
+        return action.payload
+    case "CLEAR":
+        return null
+    default:
+        return state
+  }
+}
+
+const NotificationContext = createContext()
+
+export const NotificationContextProvider = (props) => {
+  const [notification, dispatch] = useReducer(reducer, null)
+
+  return (
+    <NotificationContext.Provider value={[notification, dispatch] }>
+      {props.children}
+    </NotificationContext.Provider>
+  )
+}
+
+export const useNotificationValue = () => {
+  const valueAndDispatch = useContext(NotificationContext)
+  return valueAndDispatch[0]
+}
+
+export const useNotify = () => {
+  const valueAndDispatch = useContext(NotificationContext)
+  const dispatch = valueAndDispatch[1]
+  return (payload) => {
+    dispatch({ type: 'SET', payload})
+    setTimeout(() => {
+      dispatch({ type: 'CLEAR' })
+    }, 5000)
+  } 
+}
+
+export default NotificationContext
+
+
